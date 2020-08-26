ed6bd242c72527c165f3228696228f34a459ebe1
Vinayak Shenoy <vinayak96shenoy@gmail.com>
2020-08-26 12:48:43 +0530

    Googlenet

---
+++ googlenet/deeperGoogleNet/config/tiny_imagenet_config.py
@@ -0,0 +1,35 @@
+from os import path
+
+# define the paths to the training and validation directories
+TRAIN_IMAGES = "datasets/train"
+VAL_IMAGES = "datasets/val/images"
+
+# define the path to the file that maps validation filenames to their
+# corresponding class labels
+VAL_MAPPINGS = "datasets/val-annotations.txt"
+
+# define paths to wordnet heirarchy files which are used to generate our class
+# labels
+WORDNET_IDS = "datasets/wnids.txt"
+WORD_LABELS = "datasets/words.txt"
+
+# since we do not have access to the testing data we need to
+# take a number of images from the training data and use it instead
+NUM_CLASSES = 200
+NUM_TEST_IMAGES = 50 * NUM_CLASSES
+
+# define path to output train, val and test hdf5
+TRAIN_HDF5 = "datasets/hdf5/train.hdf5"
+VAL_HDF5 = "datasets/hdf5/val.hdf5"
+TEST_HDF5 = "datasets/hdf5/test.hdf5"
+
+# define the path to dataset mean
+DATASET_MEAN = "output/tiny-image-net-200-mean.json"
+
+# define the path to the output dir used for storing plots,
+# classification report, etc.
+OUTPUT_PATH = "output"
+MODEL_PATH = path.sep.join([OUTPUT_PATH, "checkpoints", "epoch_70.hdf5"])
+FIG_PATH = path.sep.join([OUTPUT_PATH, "deepergooglnet_tinyimagenet.png"])
+JSON_PATH = path.sep.join([OUTPUT_PATH, "deepergooglenet_tinyimagenet.json"])
+
